b8a47d73d6bdc0a6604d54934ee6d49488071d0e
EdRamos12 <40503544+EdRamos12@users.noreply.github.com>
2019-11-28 23:52:23 -0300

    script update

--- js/master.js
+++ js/master.js
@@ -1,5 +1,42 @@
 /* =======================================
-=             fade on scroll            =
+=                form js                =
+=========================================*/
+
+function enviar() {
+    var fname = document.getElementById("fname").value, femail = document.getElementById("femail").value, fmsg = document.getElementById("fmsg").value;
+    var ername = document.getElementById("ername"), eremail = document.getElementById("eremail"), ermsg = document.getElementById("ermsg");
+
+    if (fname.length > 2) {
+        document.getElementById("fname").classList.remove("form-error");
+        ername.innerHTML = " ";
+    } else {
+        console.log(fname.length)
+        ername.innerHTML = "(Insira um nome valido)";
+        document.getElementById("fname").classList.add("form-error");
+        return false;
+    }
+    if (femail.length > 5 && femail.includes("@") && femail.includes(".")) {
+        eremail.innerHTML = " ";
+        document.getElementById("femail").classList.remove("form-error");
+    } else {
+        console.log(femail.length);
+        eremail.innerHTML = "(Insira um e-mail valido)";
+        document.getElementById("femail").classList.add("form-error");
+        return false;
+    }
+    if (fmsg.length >= 10) {
+        ermsg.innerHTML = " ";
+        document.getElementById("fmsg").classList.remove("form-error");
+    } else {
+        console.log(fmsg.length);
+        ermsg.innerHTML = "(Contextualize sua mensagem)";
+        document.getElementById("fmsg").classList.add("form-error");
+        return false;
+    }
+}
+
+/* =======================================
+=            smooth scroll js            =
 =========================================*/
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -7,7 +44,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         e.preventDefault();
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
-        }, 1000);
+        });
     });
 });
 
@@ -17,9 +54,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 =========================================*/
 
 (function () {
-    var $target = $('.fade'), animationClass = 'fade-begin', offset = $(window).height() * 3 / 4;
+    
     function fadeScroll() {
-        var documentTop = $(document).scrollTop();
+        var $target = $('.fade'), animationClass = 'fade-begin', offset = $(window).height() * 3 / 4,  documentTop = $(document).scrollTop();
+        $target.each(function () {
+            var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - offset) {
+                $(this).addClass(animationClass);
+            }
+        })
+    }
+    function fadeportScroll() {
+        var $target = $('.fadeport'), animationClass = 'fade-begin', offset = $(window).height() * 3 / 4, documentTop = $(document).scrollTop();
         $target.each(function () {
             var itemTop = $(this).offset().top;
             if (documentTop > itemTop - offset) {
@@ -30,7 +76,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     fadeScroll();
     document.addEventListener('scroll', function (event) {
         fadeScroll();
-    }, true /*Capture event*/);
+        fadeportScroll();
+    }, true);
 }())
 
 /* =======================================
